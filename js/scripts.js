45a07db43373ec22cb44646526f8d502df4569c4
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-10 19:38:58 +0300

    Added the toggling effect to the classess

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
-    $(".col").click(function(){
+    $(".p1").click(function(){
+        $(show).children(".show").slidetoggle();
+        $(show).children(".show").toggle(1000);
+    });
+
+});
+$(document).ready(function(){
+    $(".p2").click(function(){
         $(".show").toggle();
     });
 
 });
 $(document).ready(function(){
-    $(".col2").click(function(){
+    $(".p3").click(function(){
         $(".show").toggle();
     });
+
+});
 
