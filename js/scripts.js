dcbaa15b026506ca0691356df1c09f4a569a5d9f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-11 01:13:08 +0300

    Added the jQuery toggle for the icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
-    $(".p1").click(function(){
-        $(show).children(".show").slidetoggle();
-        $(show).children(".show").toggle(1000);
-    });
-
-});
-$(document).ready(function(){
-    $(".p2").click(function(){
-        $(".show").toggle();
-    });
-
-});
-$(document).ready(function(){
-    $(".p3").click(function(){
-        $(".show").toggle();
-    });
-
-});
-
+  $(".design1").click(function() {
+    $(".design1").toggle(500);
+    $(".details1").toggle(500);
+  });
+  
