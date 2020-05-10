04d0aeff850b1e87f040437790b6cc864cd5dd72
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-10 19:09:47 +0300

    Added the js toggling for the cols

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".col").click(function(){
+        $(".show").toggle();
+    });
+
+});
+$(document).ready(function(){
+    $(".col2").click(function(){
+        $(".show").toggle();
+    });
+
